099e8f799d5995545abb6a7f8c715c4b30f16653
vicmcg <vkm3473@rit.edu>
2016-02-28 00:33:55 -0500

    adding all of the web shiz

--- fileGet.js
+++ fileGet.js
@@ -1,3 +1,37 @@
-var router
+var express =   require("express");
+var multer  =   require('multer');
+var app     =   express();
+var spawn 	= require("child_process").spawn;
+var process = spawn('python',["./testMain.py"]);
+var storage =   multer.diskStorage({
+	destination: function (req, file, callback) 
+		{
+    		callback(null, './uploads'); 
+    	},
+  		filename: function (req, file, callback) 
+  		{
+    		callback(null, file.fieldname + '-' + Date.now()); 
+		}
+	}
+);
 
-router.get()
+var upload = multer({ storage : storage}).single('mysteryPokemon');
+
+app.use(express.static(__dirname) )
+
+app.get('/',function(req,res){
+      res.sendFile(__dirname + "/index.html");
+});
+
+app.post('/api/photo',function(req,res){
+    upload(req,res,function(err) {
+        if(err) {
+            return res.end("Error uploading file.");
+        }
+        //res.end("File is uploaded");
+    });
+});
+
+app.listen(3000,function(){
+    console.log("Working on port 3000");
+});
---
+++ testMain.py
@@ -0,0 +1,10 @@
+import sys
+
+if __name__ == '__main__':
+	filename = 'pokemon/025.png'
+	pokemon = 'Pikachu'
+
+	f = open('updates.txt', 'w')
+	f.write(filename+'\n')
+	f.write(pokemon)
+	f.close()
